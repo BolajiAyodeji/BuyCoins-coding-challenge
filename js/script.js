7b7d22876d691652b9f3ff53756197e49f82641b
Bolaji Ayodeji <sirbeejay1@gmail.com>
2018-09-02 21:13:54 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /* 
+
 TO DO:
 ======
 
 1. When the user clicks on the button that says "Move to start", move that <article> element to the beginning of the row
 2. When the user clicks on the button that says "Delete", remove that <article> from the page entirely
 
-See https://res.cloudinary.com/bitkoin/video/upload/v1535438914/Untitled.mp4
 */
